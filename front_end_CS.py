804262f0d8e81c7f11188bfd9adb18ebde5840b8
AngeloFerrando <angelo.ferrando42@gmail.com>
2023-04-20 18:41:55 +0200

    update website

--- front_end_CS.py
+++ front_end_CS.py
@@ -769,7 +769,7 @@ def D_logic():
   st.write(str(Verif))
   st.write(str(list_pars))
   st.write(str(list_type))
-  if st.button('Next : To ...'):
+  if st.button('Next : To Model Checking'):
     (st.session_state.info_model).append([Logic,formula])
     st.session_state.cmpt_model=6
     st.experimental_rerun()
