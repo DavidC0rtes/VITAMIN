26029ab813170a9e486a2d52bce1f8d953b130e8
AngeloFerrando <angelo.ferrando42@gmail.com>
2023-04-20 17:28:55 +0200

    updating home

--- model_app.py
+++ model_app.py
@@ -132,7 +132,7 @@ if __name__=='__main__':
 
 st.markdown(" ")
 st.markdown("### ** 👨🏼‍💻 Developers and Researchers: **")
-st.image(['images/1.png'], width=110,caption=["Vadim Malvone"])
+st.image(['images/1.png', 'images/2.png'], width=110,caption=["Vadim Malvone", "Angelo Ferrando"])
 st.image(['images/2.png'], width=110,caption=["Angelo Ferrando"])
 
 
