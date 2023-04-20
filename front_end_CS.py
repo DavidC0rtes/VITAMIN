9fc538f757c6fd41a675c35edc5358d66251cbc6
AngeloFerrando <angelo.ferrando42@gmail.com>
2023-04-20 17:58:28 +0200

    update website

--- front_end_CS.py
+++ front_end_CS.py
@@ -1,8 +1,6 @@
 import streamlit as st
 from back_end_CS import *
 import os
-import os.path
-import pathlib
 
 
 
@@ -447,14 +445,9 @@ def display_case(nlp_steps):
     uploaded_file=st.file_uploader('Upload Dataset in .txt',type=['TXT'])
     if uploaded_file is not None:
       st.write('hello world')
-      data = uploaded_file.getvalue().decode('utf-8')
-      parent_path = pathlib.Path(__file__).parent.parent.resolve()
-      save_path = os.path.join(parent_path, "data")
-      complete_name = os.path.join(save_path, uploaded_file.name)
-      destination_file = open(complete_name, "w")
-      destination_file.write(data)
-      destination_file.close()
-      st.session_state["upload_state"] = "Saved " + complete_name + " successfully!"
+      with open('data/VadimTifaInter.txt', 'w') as f:
+          # st.write(dir(uploaded_file))
+          f.write(str(uploaded_file))
     else:
       filename=file_select()
       st.info('You selected {}'.format(filename))
