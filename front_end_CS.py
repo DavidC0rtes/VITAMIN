9c4a7db58ec6be33aabd13b7415b9a228189031c
AngeloFerrando <angelo.ferrando42@gmail.com>
2023-04-20 19:11:29 +0200

    update website

--- front_end_CS.py
+++ front_end_CS.py
@@ -462,7 +462,7 @@ def upload_file_handler():
 
   if filename:
     game_strategy=game(load_file=True,path1=str(filename),AW=True)
-    graph=game_strategy.display_diagram(Special_node=SP)
+    graph=game_strategy.display_diagram(Special_node=0)
     st.graphviz_chart(graph)
 
   st.write("     ")
