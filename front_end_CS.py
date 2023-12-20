5a5d97b02f5c8bf087670a15dac5b0153c08ef47
AngeloFerrando <angelo.ferrando42@gmail.com>
2023-12-20 14:02:57 +0100

    bug fix

--- front_end_CS.py
+++ front_end_CS.py
@@ -709,6 +709,7 @@ def display_MS(ms_steps):
         result  = OL.model_checking(formula, filename)
         st.write(result['res'])
       elif Logic == 'OATL':
+        st.write('here!')
         result  = OATL.model_checking(formula, filename)
         st.write(result['res'])
         st.write(result['initial_state'])
